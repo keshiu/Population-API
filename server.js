61ea8a13f28163032c2cbfd5a461528dc4e28218
Alena Arsenyeva <keshiu@list.ru>
2015-08-14 18:49:55 +0300

    Add server part and folder for client app

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require("http"),
+express = require("express"),
+app = express(),
+port = process.env.PORT||3000,
+bodyParser = require('body-parser');
+app.use(express.static(__dirname + "/client"));
+app.use(bodyParser.urlencoded({extended: true}));
+http.createServer(app).listen(port);
